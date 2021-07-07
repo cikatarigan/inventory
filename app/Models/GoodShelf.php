be2e63e4f613942f4c35e0720a818d60a8d8d347
cika_pkt <cika.pkt@gmail.com>
2021-07-07 11:28:19 +0700

    scan

---
+++ app/Models/GoodShelf.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GoodShelf extends Model
+{
+     protected $table = 'good_shelves';
+
+     protected $fillable = ['good_id', 'location_shelf_id'];
+     public $timestamps = false;
+
+     public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+}
